7882d8ab284b242cee2fb77bce06e473130d0fff
wcy <2452952178@qq.com>
2020-04-19 02:38:05 +0000

    天气显示

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.coolweather.android;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -53,7 +54,7 @@ public class ChooseAreaFragment extends Fragment {
         /**
          * 省、市、县的列表
          */
-        protected List<Province> provinceList;
+        private List<Province> provinceList;
         private List<City> cityList;
         private List<County> countyList;
         // 选中的省
@@ -87,6 +88,12 @@ public class ChooseAreaFragment extends Fragment {
                     } else if (currentLevel == LEVEL_CITY) {
                         selectedCity = cityList.get(position);
                         queryCounties();
+                    }else if (currentLevel == LEVEL_COUNTY){
+                        String weatherId = countyList.get(position).getWeatherId();
+                        Intent intent = new Intent(getActivity(),WeatherActivity.class);
+                        intent.putExtra("weather_id",weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
                     }
                 }
             });
--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -2,10 +2,11 @@ package com.coolweather.android.util;
 
 import android.text.TextUtils;
 
-
+import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
-import com.coolweather.android.db.City;
+import com.coolweather.android.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -69,5 +70,17 @@ public class Utility {
             }
         }
         return false;
+    }
+    public static Weather handleWeatherResponse(String response){
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
